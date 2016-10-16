68e79f2f289d104f0f91513ab60182bb06880f13
Lester Wang <wanghuai28@outlook.com>
2016-10-16 23:35:27 +0800

    prepare the project with react and webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * Created by 怀 on 2016/10/16.
+ */
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
